d2548d8e9721dacdb42c9d5859aa6bdb4df0c190
Daniel Sollid <dpsollid@hotmail.no>
2022-12-13 17:45:58 +0100

    updated scss

--- scripts/template/renderCarousel.mjs
+++ scripts/template/renderCarousel.mjs
@@ -1,96 +1,92 @@
-export function carouselRender(media, id, container){
-    //Carousel container - everything related to the container will be in this
-    const carouselEl = document.createElement("div");
-    carouselEl.setAttribute("id", "carousel-" + id);
-    carouselEl.setAttribute("data-bs-ride", "true");
-    carouselEl.classList.add("carousel", "slide");
-    container.append(carouselEl);
+export function carouselRender(media, id, container) {
+  //Carousel container - everything related to the container will be in this
+  const carouselEl = document.createElement("div");
+  carouselEl.setAttribute("id", "carousel-" + id);
+  carouselEl.setAttribute("data-bs-ride", "true");
+  carouselEl.classList.add("carousel", "slide");
+  container.append(carouselEl);
 
-    // carousel indicator container
-    const carouselIndicatorsEl = document.createElement("div")
-    carouselIndicatorsEl.classList.add("carousel-indicators")
-    carouselEl.append(carouselIndicatorsEl)
+  // carousel indicator container
+  const carouselIndicatorsEl = document.createElement("div");
+  carouselIndicatorsEl.classList.add("carousel-indicators");
+  carouselEl.append(carouselIndicatorsEl);
 
-    // inner carousel container
-    const innerCarouselEl = document.createElement("div")
-    innerCarouselEl.classList.add("carousel-inner")
-    carouselEl.append(innerCarouselEl);
+  // inner carousel container
+  const innerCarouselEl = document.createElement("div");
+  innerCarouselEl.classList.add("carousel-inner");
+  carouselEl.append(innerCarouselEl);
 
-    if(media.length > 0){
-        let i = 0;
-        media.forEach(img => {
-            const carouselLiEL = document.createElement("button");
-            const carouselItem = document.createElement("div");
-            const carouselImgEl = document.createElement("img") 
-            
-            carouselLiEL.setAttribute("type", "button")
-            carouselLiEL.setAttribute("data-bs-target", "carousel-" + id)
-            carouselLiEL.setAttribute("data-bs-slide-to", i)
-            carouselLiEL.setAttribute("aria-label", `Slide ${i + 1}`);
-            
+  if (media.length > 0) {
+    let i = 0;
+    media.forEach((img) => {
+      const carouselLiEL = document.createElement("button");
+      const carouselItem = document.createElement("div");
+      const carouselImgEl = document.createElement("img");
 
-            carouselItem.classList.add("carousel-item")
+      carouselLiEL.setAttribute("type", "button");
+      carouselLiEL.setAttribute("data-bs-target", "carousel-" + id);
+      carouselLiEL.setAttribute("data-bs-slide-to", i);
+      carouselLiEL.setAttribute("aria-label", `Slide ${i + 1}`);
 
-            carouselImgEl.classList.add("d-block", "w-100")
-            carouselImgEl.setAttribute("src", img)
+      carouselItem.classList.add("carousel-item");
 
-            if(i === 0){
-                carouselLiEL.classList.add("active")
-                carouselLiEL.setAttribute("aria-current", "true")
-                carouselItem.classList.add("active")
-            }
+      carouselImgEl.classList.add("d-block");
+      carouselImgEl.setAttribute("src", img);
 
-            carouselIndicatorsEl.append(carouselLiEL)
-            innerCarouselEl.append(carouselItem)
-            carouselItem.append(carouselImgEl)
-            i++
-        });
-            // Previous button
-            if(media.length !== 1 ){
-    const carouselControlPrev = document.createElement("button")
-    carouselControlPrev.classList.add("carousel-control-prev")
-    carouselControlPrev.setAttribute("type", "button")
-    carouselControlPrev.setAttribute("data-bs-slide", "prev")
-    carouselControlPrev.setAttribute("data-bs-target", "#carousel-" + id)
-    carouselEl.append(carouselControlPrev)
+      if (i === 0) {
+        carouselLiEL.classList.add("active");
+        carouselLiEL.setAttribute("aria-current", "true");
+        carouselItem.classList.add("active");
+      }
 
-    const carouselControlPrevIcon = document.createElement("span")
-    carouselControlPrevIcon.classList.add("carousel-control-prev-icon")
-    carouselControlPrevIcon.setAttribute("aria-hidden", "true")
-    carouselControlPrev.append(carouselControlPrevIcon)
+      carouselIndicatorsEl.append(carouselLiEL);
+      innerCarouselEl.append(carouselItem);
+      carouselItem.append(carouselImgEl);
+      i++;
+    });
+    // Previous button
+    if (media.length !== 1) {
+      const carouselControlPrev = document.createElement("button");
+      carouselControlPrev.classList.add("carousel-control-prev");
+      carouselControlPrev.setAttribute("type", "button");
+      carouselControlPrev.setAttribute("data-bs-slide", "prev");
+      carouselControlPrev.setAttribute("data-bs-target", "#carousel-" + id);
+      carouselEl.append(carouselControlPrev);
 
-    const carouselControlPrevText = document.createElement("span")
-    carouselControlPrevText.classList.add("sr-only")
-    carouselControlPrev.append(carouselControlPrevText)
+      const carouselControlPrevIcon = document.createElement("span");
+      carouselControlPrevIcon.classList.add("carousel-control-prev-icon");
+      carouselControlPrevIcon.setAttribute("aria-hidden", "true");
+      carouselControlPrev.append(carouselControlPrevIcon);
 
-    // Next button
-    const carouselControlNext = document.createElement("button")
-    carouselControlNext.classList.add("carousel-control-next")
-    carouselControlNext.setAttribute("type", "button")
-    carouselControlNext.setAttribute("data-bs-slide", "next")
-    carouselControlNext.setAttribute("data-bs-target", "#carousel-" + id)
-    carouselEl.append(carouselControlNext)
+      const carouselControlPrevText = document.createElement("span");
+      carouselControlPrevText.classList.add("sr-only");
+      carouselControlPrev.append(carouselControlPrevText);
 
-    const carouselControlNextIcon = document.createElement("span")
-    carouselControlNextIcon.classList.add("carousel-control-next-icon")
-    carouselControlNextIcon.setAttribute("aria-hidden", "true")
-    carouselControlNext.append(carouselControlNextIcon)
+      // Next button
+      const carouselControlNext = document.createElement("button");
+      carouselControlNext.classList.add("carousel-control-next");
+      carouselControlNext.setAttribute("type", "button");
+      carouselControlNext.setAttribute("data-bs-slide", "next");
+      carouselControlNext.setAttribute("data-bs-target", "#carousel-" + id);
+      carouselEl.append(carouselControlNext);
 
-    const carouselControlNextText = document.createElement("span")
-    carouselControlNextText.classList.add("sr-only")
-    carouselControlNext.append(carouselControlNextText)
-}
+      const carouselControlNextIcon = document.createElement("span");
+      carouselControlNextIcon.classList.add("carousel-control-next-icon");
+      carouselControlNextIcon.setAttribute("aria-hidden", "true");
+      carouselControlNext.append(carouselControlNextIcon);
 
-    } else {
-        const carouselItem = document.createElement("div");
-        const carouselImgEl = document.createElement("img")
-        carouselItem.classList.add("carousel-item", "active")
-        carouselImgEl.classList.add("d-block", "w-100")
-        carouselImgEl.setAttribute("src", "img/Gavel_0001.png")
-
-        innerCarouselEl.append(carouselItem)
-        carouselItem.append(carouselImgEl)
+      const carouselControlNextText = document.createElement("span");
+      carouselControlNextText.classList.add("sr-only");
+      carouselControlNext.append(carouselControlNextText);
     }
+  } else {
+    const carouselItem = document.createElement("div");
+    const carouselImgEl = document.createElement("img");
+    carouselItem.classList.add("carousel-item", "active");
+    carouselImgEl.classList.add("d-block", "w-100");
+    carouselImgEl.setAttribute("src", "img/Gavel_0001.png");
 
-
+    innerCarouselEl.append(carouselItem);
+    carouselItem.append(carouselImgEl);
+  }
 }
